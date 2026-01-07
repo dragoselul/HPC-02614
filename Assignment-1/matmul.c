71b7b7f32dd64117e26d51764b97fb7a02580612
Dragoselul <dragosbonaparte@gmail.com>
2026-01-07 16:24:50 +0100

    added some changes

--- Assignment-1/matmul.c
+++ Assignment-1/matmul.c
@@ -64,8 +64,26 @@ void matmult_nmk(int m, int n, int k,
 
 void matmult_nkm(int m, int n, int k,
                  double** A, double** B, double** C) {
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            C[i][j] = 0.0;
     for (int j = 0; j < n; j++)
         for (int s = 0; s < k; s++)
             for (int i = 0; i < m; i++)
                 C[i][j] += A[i][s] * B[s][j];
 }
+
+void matmult_blk(int m, int n, int k,
+                 double** A, double** B, double** C, int bs) {
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            C[i][j] = 0.0;
+    for (int i0 = 0; i0 < m; i0 += bs)
+        for (int s0 = 0; s0 < k; s0 += bs)
+            for (int j0 = 0; j0 < n; j0 += bs)
+                for (int i = i0; i < i0 + bs && i < m; i++)
+                    for (int s = s0; s < s0 + bs && s < k; s++)
+                        for (int j = j0; j < j0 + bs && j < n; j++)
+                            C[i][j] += A[i][s] * B[s][j];
+}
+
