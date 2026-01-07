bfe5b5eb5f2ad9d1dea6e0f839c801df5b50836f
Emma <emma.royant@outlook.fr>
2026-01-07 11:09:12 +0100

    Add permutations functions

--- Assignment-1/matmul.c
+++ Assignment-1/matmul.c
@@ -1,4 +1,5 @@
 #include "matmul.h"
+#include <cblas.h>
 
 void matmult_nat(int m, int n, int k, double** A, double** B, double** C)
 {
@@ -12,4 +13,71 @@ void matmult_nat(int m, int n, int k, double** A, double** B, double** C)
             }
         }
     }
+}
+
+void matmult_lib(int m, int n, int k,
+                 double** A, double** B, double** C)
+{
+    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
+                m, n, k, 1.0, A[0], k, B[0], n, 0.0, C[0], n);
+    
+}
+
+// The first is basically the same as matmult_nat
+void matmult_mnk(int m, int n, int k,
+                 double** A, double** B, double** C)
+{
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++) {
+            C[i][j] = 0.0;
+            for (int s = 0; s < k; s++)
+                C[i][j] += A[i][s] * B[s][j];
+        }
+}
+
+void matmult_mkn(int m, int n, int k,
+                 double** A, double** B, double** C)
+{
+    for (int i = 0; i < m; i++)
+        for (int s = 0; s < k; s++)
+            for (int j = 0; j < n; j++)
+                C[i][j] += A[i][s] * B[s][j];
+}
+
+void matmult_knm(int m, int n, int k,
+                 double** A, double** B, double** C)
+{
+    for (int s = 0; s < k; s++)
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                C[i][j] += A[i][s] * B[s][j];
+}
+
+void matmult_kmn(int m, int n, int k,
+                 double** A, double** B, double** C)
+{
+    for (int s = 0; s < k; s++)
+        for (int j = 0; j < n; j++)
+            for (int i = 0; i < m; i++)
+                C[i][j] += A[i][s] * B[s][j];
+}
+
+void matmult_nmk(int m, int n, int k,
+                 double** A, double** B, double** C)
+{
+    for (int j = 0; j < n; j++)
+        for (int i = 0; i < m; i++) {
+            C[i][j] = 0.0;
+            for (int s = 0; s < k; s++)
+                C[i][j] += A[i][s] * B[s][j];
+        }
+}
+
+void matmult_nkm(int m, int n, int k,
+                 double** A, double** B, double** C)
+{
+    for (int j = 0; j < n; j++)
+        for (int s = 0; s < k; s++)
+            for (int i = 0; i < m; i++)
+                C[i][j] += A[i][s] * B[s][j];
 }
