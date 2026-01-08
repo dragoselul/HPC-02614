5d0bc189f54c5f749e45eb3f016a1f1551d23832
Emma <emma.royant@outlook.fr>
2026-01-08 10:39:49 +0100

    Add zeroing C in permutations

--- Assignment-1/matmul.c
+++ Assignment-1/matmul.c
@@ -31,6 +31,11 @@ void matmult_mnk(int m, int n, int k,
 
 void matmult_mkn(int m, int n, int k,
                  double** A, double** B, double** C) {
+    
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            C[i][j] = 0.0;
+                
     for (int i = 0; i < m; i++)
         for (int s = 0; s < k; s++)
             for (int j = 0; j < n; j++)
@@ -39,6 +44,11 @@ void matmult_mkn(int m, int n, int k,
 
 void matmult_kmn(int m, int n, int k,
                  double** A, double** B, double** C) {
+    
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            C[i][j] = 0.0;
+
     for (int s = 0; s < k; s++)
         for (int i = 0; i < m; i++)
             for (int j = 0; j < n; j++)
@@ -47,6 +57,11 @@ void matmult_kmn(int m, int n, int k,
 
 void matmult_knm(int m, int n, int k,
                  double** A, double** B, double** C) {
+    
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            C[i][j] = 0.0;
+
     for (int s = 0; s < k; s++)
         for (int j = 0; j < n; j++)
             for (int i = 0; i < m; i++)
@@ -65,9 +80,11 @@ void matmult_nmk(int m, int n, int k,
 
 void matmult_nkm(int m, int n, int k,
                  double** A, double** B, double** C) {
+                    
     for (int i = 0; i < m; i++)
         for (int j = 0; j < n; j++)
             C[i][j] = 0.0;
+
     for (int j = 0; j < n; j++)
         for (int s = 0; s < k; s++)
             for (int i = 0; i < m; i++)
