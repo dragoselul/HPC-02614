abdb19135165bd295504af8a67bb849fb7ed4560
sh234929 <s234929@dtu.dk>
2026-01-14 16:17:01 +0100

    New initialize function

--- Assignment-2/init.c
+++ Assignment-2/init.c
@@ -1,10 +1,86 @@
 #include <math.h>
 #include "init.h"
+#include <stdio.h>
 
+void print_tesor(double*** A, int N)
+{
+    for(int i = 0; i < N; i++)
+    {
+        printf("Layer %d\n", i);
+        for(int j = 0; j < N; j++)
+        {
+            for(int k = 0; k < N; k++)
+            {
+                printf("%f ", A[i][j][k]);
+            }
+            printf("\n");
+        }
+    }
+}
 void initialize(double ***u, double ***f, int N, double start_T)
 {
-    double h = 2.0 / (N - 1);
+    double h = 2.0 / (N-1);
 
+    for (int i = 0; i < N; i++) 
+    {
+        double x = -1.0 + i * h;
+
+        for (int j = 0; j < N; j++) {
+            double y = -1.0 + j * h;
+
+            for (int k = 0; k < N; k++) {
+                double z = -1.0 + k * h;
+
+                // Default initial interior value
+                u[i][j][k] = start_T;
+                f[i][j][k] = 0.0;
+            }
+        }
+    }
+    // radiator indecies
+    int r_xb = (int)ceil(0);
+    int r_xe = (int)floor(5*(double)(N-1)/16) +1;
+    int r_yb = (int)ceil(0);
+    int r_ye = (int)floor((double)(N-1)/4) +1;
+    int r_zb = (int)ceil((double)(N-1)/6);
+    int r_ze = (int)floor((double)(N-1)/2) +1;
+    // radiator
+    for(int i = r_xb; i < r_xe; i++)
+    {
+        for(int j = r_yb; j < r_ye; j++)
+        {
+            for(int k = r_zb; k < r_ze; k++)
+            {
+                f[i][j][k]   = 200;
+            }
+        }
+    }
+
+
+    // Dirichlet boundary conditions
+    for(int j = 0; j < N; j++)
+    {
+        for(int k = 0; k < N; k++)
+        {
+            u[0][j][k]   = 20;
+            u[N-1][j][k] = 20;
+        }
+    }
+    for(int i = 0; i < N; i++)
+    {
+        for(int j = 0; j < N; j++)
+        {
+            u[i][j][0]   = 20;
+            u[i][j][N-1] = 20;
+        }
+        for(int k = 0; k < N; k++)
+        {
+            u[i][0][k]   = 0;
+            u[i][N-1][k] = 0;
+        }
+    }
+
+    /*
     for (int i = 0; i < N; i++) {
         double x = -1.0 + i * h;
 
@@ -19,7 +95,7 @@ void initialize(double ***u, double ***f, int N, double start_T)
                 f[i][j][k] = 0.0;
                 
                 // Dirichlet boundary conditions
-                if (j == N - 1) {          // y = +1 wall
+                if (j == N-1) {          // y = +1 wall
                     u[i][j][k] = 20.0;
                     continue;
                 }
@@ -27,8 +103,8 @@ void initialize(double ***u, double ***f, int N, double start_T)
                     u[i][j][k] = 0.0;
                     continue;
                 }
-                if (i == 0 || i == N - 1 ||  // x = ±1 walls
-                    k == 0 || k == N - 1) {  // z = ±1 walls
+                if (i == 0 || i == N-1 ||  // x = ±1 walls
+                    k == 0 || k == N-1 ) {  // z = ±1 walls
                     u[i][j][k] = 20.0;
                 }
 
@@ -42,4 +118,5 @@ void initialize(double ***u, double ***f, int N, double start_T)
             }
         }
     }
+    */
 }
