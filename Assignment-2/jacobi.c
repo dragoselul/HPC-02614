f1bf9b89a168fe11c362c5d57df075a91227f4e1
sh234929 <s234929@dtu.dk>
2026-01-15 13:37:37 +0100

    Fixed jacobi.c

--- Assignment-2/jacobi.c
+++ Assignment-2/jacobi.c
@@ -29,13 +29,18 @@ int jacobi(double ***u, double ***u_new, double ***f, int N, int iter_max, doubl
                     double diff = fabs(new_val - u[i][j][k]);
                     if (diff > d) d = diff;
 
-                    u[i][j][k] = new_val;
-                    //u_new[i][j][k] = new_val;
+                    u_new[i][j][k] = new_val;
                 }
-
-        //double ***tmp = u;
-        //u = u_new;
-        //u_new = tmp;
+        for(int i = 1; i < N-1; i++)
+        {
+            for (int j = 1; j < N-1; j++)
+            {
+                for (int k = 1; k < N-1; k++) 
+                {
+                    u[i][j][k] = u_new[i][j][k];
+                }
+            }
+        }
 
         it++;
 
