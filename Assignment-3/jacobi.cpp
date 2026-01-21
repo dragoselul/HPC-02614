a65ee85a2a1ec887debafcb42bb9b6c6b4c3ff97
Emma <emma.royant@outlook.fr>
2026-01-21 10:31:47 +0100

    Changes in Jacobi

--- Assignment-3/jacobi.cpp
+++ Assignment-3/jacobi.cpp
@@ -55,3 +55,30 @@ int jacobi_omp(double*** u, double*** u_new, double*** f, int N, int iter_max, d
     }
     return it;
 }
+
+int jacobi_gpu(double*** u, double*** u_new, double*** f, int N, int iter_max, double* tolerance) {
+    
+    double delta2 = (2.0 / (N - 1)) * (2.0 / (N - 1));
+    int it = 0;
+
+    while (it < iter_max) {
+
+        #pragma omp parallel for collapse(3) reduction(max:d) schedule(static)
+        for (int i = 1; i < N-1; i++)
+            for (int j = 1; j < N-1; j++)
+                for (int k = 1; k < N-1; k++) {
+                    
+                    double val = (1.0/6.0) * (
+                        u[i-1][j][k] + u[i+1][j][k] +
+                        u[i][j-1][k] + u[i][j+1][k] +
+                        u[i][j][k-1] + u[i][j][k+1] +
+                        delta2 * f[i][j][k]);
+                    
+                    u_new[i][j][k] = val;
+                }
+
+        double*** tmp = u; u = u_new; u_new = tmp;
+        it++;
+    }
+    return it;
+}
